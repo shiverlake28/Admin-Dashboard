4dc3980936ef3bb853d97f4a0187f39c7cf0d0df
shiver_lake28 <adityapasari278@gmail.com>
2026-07-16 14:04:01 +0530

    new file

---
+++ revision.py
@@ -0,0 +1,11 @@
+basket = [1,2,3,4,2]
+print(basket.index(4))
+print(basket.count(2))
+basket.pop(0)
+print(basket)
+basket.remove(2)
+print(basket)
+list0 =  [1,2,3,4,5]
+print(list(range(1, 10)))
+a,b,c, * other = [1,2,3,4,5,6,4,7]
+print(other)
